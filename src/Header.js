3915ac1a2597d20f583abc7cf2ba71a333a7f954
codeguruvaa <mohanvej@gmail.com>
2022-07-05 13:45:37 +0530

    init

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+
+           <header>
+                <nav>Home | ABout | Services | Contact</nav>
+           </header>
+            
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/PositionsCls.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './style.css';
+
+function PositionsCls() {
+  return (
+    <div>
+
+    <section>
+      <div className='secOne'>
+        <div className='boxOne'></div>
+        <div className='btn'></div>
+      </div>
+
+      <div className='secTwo'>
+        <div className='boxOne'></div>
+        <div className='btn'></div>
+      </div>
+
+      <div className='secTwo'>
+        <div className='boxOne'></div>
+        <div className='btn'></div>
+      </div>
+
+      <div className='secTwo'>
+        <div className='boxOne'></div>
+        <div className='btn'></div>
+      </div>
+    </section>
+
+    </div>
+  );
+}
+
+export default PositionsCls;
